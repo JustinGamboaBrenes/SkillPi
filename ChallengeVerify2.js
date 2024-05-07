ec5fde5b9a839a8e68a7fafa64f363bd7b378c98
JustinGamboaBrenes <136771437+JustinGamboaBrenes@users.noreply.github.com>
2024-05-07 03:18:26 -0600

    Update ChallengeVerify2.js

--- ChallengeVerify2.js
+++ ChallengeVerify2.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const { promisify } = require('util');
-const readFileAsync = promisify(fs.readFile);
+const filePath = '/home/candidate/javascript_advanced.html';
 
-async function verificarReto2(filePath) {
-  try {
-    const codigo = await readFileAsync(filePath, 'utf8');
-    const reto2 = /setTimeout\([\s\S]*, 2000\)/.test(codigo);
-    console.log(`Reto 2 cumplido: ${reto2}`);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error:', err);
+        process.exit(1);
+    }
+    const reto2 = /setTimeout\([\s\S]*, 2000\)/.test(data);
+    console.log(`Reto 2 cumplido: ${reto2 ? 'success' : 'failure'}`);
+    process.exit(reto2 ? 0 : 1);
+});
 
-if (require.main === module) {
-  verificarReto2('/home/candidate/javascript_advanced.html');
-}
-
-module.exports = verificarReto2;
