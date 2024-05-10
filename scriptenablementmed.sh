7c98cd26477a486605a34f8c396d51fa5be7d5e6
JustinGamboaBrenes <136771437+JustinGamboaBrenes@users.noreply.github.com>
2024-05-10 07:15:17 -0600

    Update scriptenablementmed.sh

--- scriptenablementmed.sh
+++ scriptenablementmed.sh
@@ -6,5 +6,5 @@ DOCKER_HOST_IP=$(curl ipinfo.io/ip)
 echo "Retrieved IP address: $DOCKER_HOST_IP"
 
 echo "Substituting IP address in index.html..."
-sed -i "s/138.94.56.72/${DOCKER_HOST_IP}/g" /var/www/html/index.html
+#sed -i "s/138.94.56.72/${DOCKER_HOST_IP}/g" /var/www/html/index.html
 echo "Substitution completed."
